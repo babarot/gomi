aa6dcd6278cb91cc04f1ce97d62b47d168372513
b4b4r07 <b4b4r07@gmail.com>
2015-07-02 17:58:57 +0900

    Replace jessevdk/go-flags with flag

--- cmd/gomi/gomi.go
+++ cmd/gomi/gomi.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
 
 	"github.com/b4b4r07/gomi"
-	"github.com/jessevdk/go-flags"
 )
 
 type Options struct {
@@ -18,41 +18,54 @@ var opts Options
 
 func main() {
 	// Parse arguments
-	args, err := flags.Parse(&opts)
-	if err != nil {
+	var version, system, restore bool
+	flags := flag.NewFlagSet("gch", flag.ContinueOnError)
+	flags.SetOutput(os.Stderr)
+	flags.Usage = func() {
+		fmt.Fprint(os.Stderr, "help")
+	}
+
+	flags.BoolVar(&version, "version", false, "")
+	flags.BoolVar(&restore, "restore", false, "")
+	flags.BoolVar(&restore, "r", false, "")
+	flags.BoolVar(&system, "system", false, "")
+	flags.BoolVar(&system, "s", false, "")
+
+	// Parse all the flags
+	if err := flags.Parse(os.Args[1:]); err != nil {
 		os.Exit(1)
 	}
 
-	err = gomi.Init()
+	err := gomi.Init()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "gomi: ", err)
 		os.Exit(1)
 	}
 
-	// Restore Mode
-	if opts.Restore {
+	if restore {
 		var location string
-		if len(args) > 0 {
-			location = args[0]
+		if flags.NArg() > 0 {
+			location = flags.Args()[0]
 		}
-
 		if err := gomi.Restore(location); err != nil {
 			fmt.Fprintln(os.Stderr, "gomi: ", err)
 			os.Exit(1)
 		}
+		os.Exit(1)
+	} else if version {
+		fmt.Fprintln(os.Stderr, "version")
 		os.Exit(0)
 	}
 
-	// Check arguments
-	if len(args) == 0 {
+	if flags.NArg() < 1 {
 		fmt.Fprintln(os.Stderr, "gomi: ", fmt.Errorf("too few arguments"))
 		os.Exit(1)
 	}
 
 	var location, trashcan string
 
-	for _, arg := range args {
-		if opts.System {
+	for _, arg := range flags.Args() {
+		if system {
 			trashcan, err = gomi.System(arg)
 		} else {
 			trashcan, err = gomi.Remove(arg)
@@ -70,3 +83,14 @@ func main() {
 		}
 	}
 }
+
+var helpText = `Usage: gomi [options] [path]
+  gomi is a simple trash tool, easy to restore and preview
+
+Options:
+  --restore, -r      Restore the files that removed by gomi
+                     with peco-like UI.
+  --system, -s       Remove with system trash can
+                     It supports only Macintosh.
+  --version          Print the version of this application
+`
